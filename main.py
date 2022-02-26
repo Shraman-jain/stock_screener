5df8bc89047d229bcfceb354d2f2c938b96f852d
Shraman-jain <60072287+Shraman-jain@users.noreply.github.com>
2022-02-26 15:32:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ end = dt.datetime.now()
 
 st.title('STOCK SCREENER')
 
-strgy = st.selectbox("STRATEGY LIST",('None','ABC', '44MA', 'BOLLINGER BAND (DAILY)','ATH','15 MIN BUY','15 MIN SELL'))
+strgy = st.selectbox("STRATEGY LIST",('None','ABC', '44MA', 'BOLLINGER BAND','ATH','15 MIN BUY','15 MIN SELL'))
 st.write("selected",strgy)
 sc_list = st.selectbox("SCRIPT LIST",('None','Nifty 500', 'Large Cap', 'Mid Cap','Small Cap'))
 st.write("selected",sc_list)
