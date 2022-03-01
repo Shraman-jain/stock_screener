081fcd1c960974eafa37f9df3205a88fe6928562
Shraman-jain <60072287+Shraman-jain@users.noreply.github.com>
2022-03-02 00:40:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ import pandas_datareader.data as pdr
 import math
 yf.pdr_override()
 
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
+
 
 def get_sma(prices,rate):
   return prices.rolling(rate).mean()
@@ -491,3 +490,5 @@ if(st.button("Start Screening")):
         st.text("Select some strategy ")  
 else:
     st.text("click on start Screening")      
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
