fcf29a5e11f1e76ad8d052b5e34267273a5463b0
Shraman-jain <60072287+Shraman-jain@users.noreply.github.com>
2022-03-11 01:19:07 +0530

    Update pyweb_demo.py

--- pyweb_demo.py
+++ pyweb_demo.py
@@ -1,5 +1,15 @@
-an={}
-for i in range(3,10):
-    an[i]={'i':i**2,
-            'j':25}
-print(an)
+import streamlit as st
+
+st.session_state
+st.title('Cal')
+
+r=st.text_input('Risk',key='r')
+ep=st.text_input('Entry Price',key='w')
+
+incr = st.button('Increment')
+dec = st.button('DEC')
+if incr:
+    st.write(st.session_state.r+st.session_state.ep)
+
+if dec:
+    st.write(st.session_state.ep+st.session_state.r)
