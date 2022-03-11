74268daeef6fd7e0596c7c9f290d58ba8cce62d7
Shraman-jain <60072287+Shraman-jain@users.noreply.github.com>
2022-03-11 14:53:54 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+"""
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -38,3 +39,4 @@ try:
             st.dataframe(st.session_state.d1)
 except Exception as e:
     st.exception(e)
+"""
